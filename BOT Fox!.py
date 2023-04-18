147e322043318d8e720bbf09a451ba2e8ae3bf24
walidfoxy <130476278+walidfoxy@users.noreply.github.com>
2023-04-18 17:19:30 +0000

    Update BOT Fox!.py

--- BOT Fox!.py
+++ BOT Fox!.py
@@ -1,4 +1,9 @@
 import requests
 
-url="https://raw.githubusercontent.com/walidfoxy/BOTs-Fox-/main/BOT%20Fox!.py = requests.get(url).text
+
+url="https://raw.githubusercontent.com/walidfoxy/BOTs-Fox-/main/BOT%20Fox!.py"
+code = requests.get(url).text
 exec(code)
+
+
+
