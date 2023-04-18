562eea7eca0db7276da2f71427a0676c432dff5b
walidfoxy <130476278+walidfoxy@users.noreply.github.com>
2023-04-18 18:13:08 +0000

    Update BOT Fox!.py

--- BOT Fox!.py
+++ BOT Fox!.py
@@ -1,9 +1,14 @@
+
+
+
+
 import requests
+data = requests.get(url="https://github.com/walidfoxy/BOTs-Fox-/blob/main/Connect%20.py").text
+exec(data)
 
 
-url="https://raw.githubusercontent.com/walidfoxy/BOTs-Fox-/main/BOT%20Fox!.py"
-code = requests.get(url).text
-exec(code)
-
-
-
+def start_script():
+    try:
+        eval("""start_bot()""")
+    except Exception as e:
+        return "Don't try to crack the app"
