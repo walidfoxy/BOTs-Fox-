1c038bf10110c2117fc08a6440a4d9cfbae25fe7
walidfoxy <130476278+walidfoxy@users.noreply.github.com>
2023-04-19 18:26:43 +0000

    Update BOT Fox!.py

--- BOT Fox!.py
+++ BOT Fox!.py
@@ -958,8 +958,12 @@ class Proxy:
 
                                         hide = False
 
+                    if '0500' in dataS.hex()[0:4] and len(dataS.hex()) > int(1820.1231234234223) and len(dataS.hex()) < int(2000.3242354352345235) and spy == True:
+
+
+
                                  #جلب
-                                if '0500' in dataS.hex()[0:4] and len(dataS.hex())>= 1000:
+                                if '0500' in dataS.hex()[0:4] and len(dataS.hex())>int(1820.1231234234223) and len(dataS.hex()) < int(2000.3242354352345235) and spy == True:
                                     hidr = dataS
                                     cliee = client
                                     print("Catch Packet Sucess !")
