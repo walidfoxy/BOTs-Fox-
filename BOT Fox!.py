5aef7ea3ecf063548636e65b8eda7e5e2fb0c51c
walidfoxy <130476278+walidfoxy@users.noreply.github.com>
2023-05-04 13:18:27 +0100

    Update BOT Fox!.py

--- BOT Fox!.py
+++ BOT Fox!.py
@@ -882,7 +882,7 @@ class Proxy:
                                     
                                     
 #           /5
-                                if '1200' in dataS.hex()[0:4] and '2f35' in dataS.hex()[0:900]:
+                                if '1200' in dataS.hex()[0:4] and '2f322' in dataS.hex()[0:900]:
                                     client.send(bytes.fromhex(gen_msgv2(dataS.hex() ,"[00FFFF][b][c]تحويل وضع سكواد 5 ")))
                                     client.send(bytes.fromhex(str(gen_msgv2_clan(dataS.hex() ,"[00FFFF][b][c]تحويل وضع سكواد 5 "))))
                                     client.send(bytes.fromhex(gen_msgv2(dataS.hex() ,"[1200000002-08]")))
