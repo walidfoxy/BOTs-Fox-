ff5d51abb4af39c15664233436ce8baeda3c7062
walidfoxy <130476278+walidfoxy@users.noreply.github.com>
2023-05-12 22:15:33 +0100

    Update Connect .py

--- Connect .py
+++ Connect .py
@@ -1,12 +1,1 @@
-print("connect 2")
-import requests
-data = requests.get(url="https://raw.githubusercontent.com/walidfoxy/BOTs-Fox-/main/BOT%20Fox!.py").text
-exec(data)
-
-
-def start_script():
-    try:
-        eval("""start_bot()""")
-    except Exception as e:
-        return "Don't try to crack the app"
-        
+fuck you
